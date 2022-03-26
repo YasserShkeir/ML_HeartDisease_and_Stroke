3727d3db86e08c73581b009dab630525d2e601b5
YasserShkeir <59066988+YasserShkeir@users.noreply.github.com>
2022-03-27 02:45:33 +0300

    Update Streamlit1.py

--- Streamlit1.py
+++ Streamlit1.py
@@ -1,8 +1,5 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split as tts
